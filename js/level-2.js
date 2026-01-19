f67f4204c5ff96fbaf440778502add6fd987c8af
Andrii-Mitko <andreymit123@gmail.com>
2026-01-19 21:38:18 +0200

    Initial commit

---
+++ js/level-2.js
@@ -0,0 +1,41 @@
+// // // Напиши функцію, яка приймає масив та виводить кожен елемент масиву у консоль.
+// function abd(a) {
+//   for (let i = 0; i <= a.length - 1; i++) {
+//     console.log(a[i]);
+//   }
+// }
+// abd([4, 2, 3]);
+// const rtt = ['histry', 'ory', 'f', 2, 8, 7];
+// console.log(rtt[rtt.length - 2]);
+
+// Напиши функцію, яка приймає масив чисел та виводить лише парні числа у консоль.
+
+// function abd(a) {
+//   const np = [];
+//   const p = [];
+//   for (let i = 0; i <= a.length - 1; i++) {
+//     if (a[i] % 2 === 0) {
+//       p.push(a[i]);
+//     } else {
+//       np.push(a[i]);
+//     }
+//   }
+//   console.log(p);
+//   console.log(np);
+
+//   return p.concat(np);
+
+// abd([4, 2, 3, 7, 8, 9, 4, 5, 6, 1, 2, 50, 70, 80, 99, 3]);
+// console.log(abd([4, 2, 3, 7, 8, 9, 4, 5, 6, 1, 2, 3]));
+
+// function createArrayOfNumbers(min, max) {
+//   const rez = [];
+//   for (let i = min; i <= max; i++) {
+//     return (rez = [i]);
+//   }
+// }
+// function demo() {
+//   console.log(arguments);
+// }
+
+// demo(10, 20, 30);
